6ce6bcc6829c74f389dc27a7aa2de2bee3877e23
Owner <owner@kevin-mbp.myfiosgateway.com>
2021-03-20 00:50:59 -0400

    Done

---
+++ BMI.py
@@ -0,0 +1,8 @@
+print("Welcome to BMI: Body Mass Index Calculator v.0.1")
+userWeight = input("Enter your weight (in pounds): ")
+userHeight = input("Enter your height (in inches): ")
+userHeightFloat = float(userHeight)
+
+myBMIpy = round((703 * float(userWeight)) / (userHeightFloat * userHeightFloat), 2)
+
+print("Your body mass index (BMI) is " + str(myBMIpy) + "%")
